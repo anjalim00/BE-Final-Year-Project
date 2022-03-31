bceb895f74080c71bdf18ac9b564c70626fcf0d5
kevin khimasia <59766950+kevinkhimasia@users.noreply.github.com>
2022-03-31 22:56:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from asyncio.windows_events import NULL
+from fileinput import filename
+import imp
 from ipaddress import ip_address
 from flask_mail import Mail
 from flask import Flask, jsonify, render_template,redirect, request,session, url_for
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 # from passlib.hash import sha256_crypt
 import re
 import nltk
@@ -10,6 +14,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 import PyPDF2
 import os
+import docx2txt
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -24,8 +29,9 @@ app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"] = "******"
 app.config["MAIL_PASSWORD"] = "******"
 app.config["MAIL_USE_SSL"] = True
-app.config["UPLOAD_FOLDER"] = "C:\\Users\\Dell\\Desktop\\Text_summarization\\static"
+app.config["UPLOAD_FOLDER"] = "C:\\Users\\Dell\\Desktop\\text_summarizer\\BE-Final-Year-Project\\static"
 mail = Mail(app)
+mysql = MySQL(app)
 
 class Registration(db.Model):
     first_name = db.Column(db.String(80), unique=False, nullable=False)
@@ -37,6 +43,16 @@ class Registration(db.Model):
 class IpAddressDetails(db.Model):
     ip_address = db.Column(db.String(20), unique=True, nullable=False,primary_key=True)
     count = db.Column(db.String(10),unique=False,nullable=False)
+
+class UserHistory(db.Model):
+    sr_no = db.Column(db.Integer(),nullable=False,primary_key=True)
+    email = db.Column(db.String(20),unique=True,nullable=False)
+    notes_title = db.Column(db.String(30),unique=False,nullable=True)
+    notes = db.Column(db.String(100),unique=False,nullable=True)
+    summary_title = db.Column(db.String(30),unique=False,nullable=True)
+    summary = db.Column(db.String(100),unique=True,nullable=True)
+    date = db.Column(db.DateTime)
+    
 @app.route("/",methods=['GET','POST'])
 def home():
     if request.method == "POST":
@@ -53,6 +69,9 @@ def home():
                 limitReached = "Sorry you have reached the maximum limit of 3. If you wish to use our tools then kindly register on our website"
                 return render_template("index.html",limitReached=limitReached,temp=temp)
         textInput = request.form.get("paragraphInput")
+        if session.get('email') is None and len(textInput)>1000:
+            wordSizeLimit = "Length of the input should not exceed 1000 words"
+            return render_template("index.html",wordSizeLimit=wordSizeLimit)
         stopWords = set(stopwords.words("english"))
         words = word_tokenize(textInput)
         freqTable = dict()
@@ -88,7 +107,9 @@ def home():
         list1 = sent_tokenize(summary)
         count=0
         count=count+1
-        
+        if session.get('email') is not None:
+            email = session.get('email')
+            entry = UserHistory(email=email,)
         if session.get("email") is None:
             if record is None:
                 entry = IpAddressDetails(ip_address=ipAddress,count=count)
@@ -199,20 +220,27 @@ def contact():
 @app.route("/profile", methods=['GET','POST'])
 def profile():
     if request.method == "POST":
-        name = request.form.get('type')
-        record = User_history.query.filter_by(email=session.get("email")).all()
-        if record is not None:
-            return render_template("profile.html", record=record, type=name)
+        name1 = request.form.get('Summary')
+        name2 = request.form.get('Notes')
+        email=session.get('email')
+        records = UserHistory.query.filter_by(email=email).all()
+        if len(records) is not NULL:
+            if name1 is None:
+                return render_template("profile.html", records=records,name2=name2)
+            else:
+                return render_template("profile.html", records=records, name1=name1)
+        else:
+            return render_template("profile.html",records=records)
     return render_template("profile.html")
 
 @app.route('/check_selected', methods=['GET', 'POST'])
-def check_selected():
+def checkSelected():
     title_id = request.args.get('title', 0, type=int)
-    record = User_history.query.get(title_id)
-    if record.previous_notes is not None :
-        return jsonify(record=record.previous_notes)
+    record1 = UserHistory.query.get(title_id)
+    if record1.notes is not None :
+        return jsonify(record1=record1.notes)
     else:
-        return jsonify(record=record.previous_summaries)
+        return jsonify(record1=record1.summary)
 
 # @app.route("/extractive")
 # def extractiveSummarization():
@@ -232,63 +260,75 @@ def getDataFromFile():
                 return render_template("index.html",limitReached=limitReached,temp=temp)
         # creating a pdf file object
         f = request.files['filename']
-        f.save(os.path.join(app.config["UPLOAD_FOLDER"],secure_filename(f.filename)))
-        pdfFileObj = open(f"C:\\Users\\Dell\\Desktop\\Text_summarization\\static\\{f.filename}", 'rb')
-        
-        # creating a pdf reader object
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-        
-        # printing number of pages in pdf file
-        print(pdfReader.numPages)
-        
-        # creating a page object
-        pageObj = pdfReader.getPage(0)
-        
-        # extracting text from page
-        textInput = pageObj.extractText()
-        stopWords = set(stopwords.words("english"))
-        words = word_tokenize(textInput)
-        freqTable = dict()
-        for word in words:
-            word = word.lower()
-            if word in stopWords:
-                continue
-            if word in freqTable:
-                freqTable[word] += 1
+        fileName = f.filename
+        file_size = os.path.getsize(fileName)
+        if session.get('email') is None and file_size>10000000:
+            fileSizeError = "File size should not exceed 1MB"
+            return render_template("index.html",fileSizeError=fileSizeError)
+        if fileName.lower().endswith('.pdf') or fileName.lower().endswith('.docx'):
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"],secure_filename(fileName)))
+            if fileName.lower().endswith('.docx'):
+                textInput = docx2txt.process("kevin1.docx")
             else:
-                freqTable[word] = 1
-        sentences = sent_tokenize(textInput)
-        sentenceValue = dict()
-        for sentence in sentences:
-            for word, freq in freqTable.items():
-                if word in sentence.lower():
-                    if sentence in sentenceValue:
-                        sentenceValue[sentence] += freq
-                    else:
-                        sentenceValue[sentence] = freq
-        sumValues = 0
-        for sentence in sentenceValue:
-            sumValues += sentenceValue[sentence]
-        average = int(sumValues / len(sentenceValue))
-        summary = ''
-        for sentence in sentences:
-            if len(textInput)>=100 and len(textInput)<400:
-                if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.1 * average)):
-                    summary += " " + sentence
-            elif len(textInput)>=400:
-                if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
-                    summary += " " + sentence
-        list1 = sent_tokenize(summary)
-        count=0
-        count=count+1
-        if session.get("email") is None:
-            if record is None:
-                entry = IpAddressDetails(ip_address=ipAddress,count=count)
-                db.session.add(entry)
-                db.session.commit()
-            else:
-                record.count = int(record.count) + 1
-                db.session.commit()
-        return render_template("index.html",summary=summary,list1=list1)
+                pdfFileObj = open(f"C:\\Users\\Dell\\Desktop\\text_summarizer\\BE-Final-Year-Project\\{fileName}", 'rb')
+                
+                # creating a pdf reader object
+                pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+                
+                # printing number of pages in pdf file
+                print(pdfReader.numPages)
+                
+                # creating a page object
+                pageObj = pdfReader.getPage(0)
+                
+                # extracting text from page
+                textInput = pageObj.extractText()
+            stopWords = set(stopwords.words("english"))
+            words = word_tokenize(textInput)
+            freqTable = dict()
+            for word in words:
+                word = word.lower()
+                if word in stopWords:
+                    continue
+                if word in freqTable:
+                    freqTable[word] += 1
+                else:
+                    freqTable[word] = 1
+            sentences = sent_tokenize(textInput)
+            sentenceValue = dict()
+            for sentence in sentences:
+                for word, freq in freqTable.items():
+                    if word in sentence.lower():
+                        if sentence in sentenceValue:
+                            sentenceValue[sentence] += freq
+                        else:
+                            sentenceValue[sentence] = freq
+            sumValues = 0
+            for sentence in sentenceValue:
+                sumValues += sentenceValue[sentence]
+            average = int(sumValues / len(sentenceValue))
+            summary = ''
+            for sentence in sentences:
+                if len(textInput)>=100 and len(textInput)<400:
+                    if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.1 * average)):
+                        summary += " " + sentence
+                elif len(textInput)>=400:
+                    if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
+                        summary += " " + sentence
+            list1 = sent_tokenize(summary)
+            count=0
+            count=count+1
+            if session.get("email") is None:
+                if record is None:
+                    entry = IpAddressDetails(ip_address=ipAddress,count=count)
+                    db.session.add(entry)
+                    db.session.commit()
+                else:
+                    record.count = int(record.count) + 1
+                    db.session.commit()
+            return render_template("index.html",summary=summary,list1=list1)
+        else:
+            supportedFileFormats = "Supported Files Formats are PDF and WORD files"
+            return render_template("index.html",supportedFileFormats=supportedFileFormats)
         
 app.run(debug=True)
